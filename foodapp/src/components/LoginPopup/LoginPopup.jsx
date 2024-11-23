b80fa1ce7ceb1f2fff2b91e3dc411944a7fb68da
Andy Ríos <anrioss05@gmail.com>
2024-11-23 01:10:20 -0600

    Se inicia la página del carrito

--- foodapp/src/components/LoginPopup/LoginPopup.jsx
+++ foodapp/src/components/LoginPopup/LoginPopup.jsx
@@ -3,19 +3,46 @@ import "./LoginPopup.css";
 import { assets } from "../../assets/frontend_assets/assets";
 
 const LoginPopup = ({ setShowLogin }) => {
-  const [currSate, setCurrSate] = useState("Sign Up");
+  const [currSate, setCurrSate] = useState("Login");
 
   return (
     <div className="login-popup">
       <from className="login-popup-container">
-        <dic className="login-popup-title">
+        <div className="login-popup-title">
           <h2>{currSate}</h2>
           <img
             onClick={() => setShowLogin(false)}
             src={assets.cross_icon}
             alt=""
           />
-        </dic>
+        </div>
+        <div className="login-popup-inputs">
+          {currSate === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Your Name" required />
+          )}
+          <input type="email" placeholder="Your Email" required />
+          <input type="password" placeholder="Your Password" required />
+        </div>
+        <button>
+          {currSate === "Sign Up" ? "Create an account" : "Login"}
+        </button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By signing up, you agree to our terms of use and privacy policy</p>
+        </div>
+        {currSate === "Login" ? (
+          <p>
+            Create a new account?{" "}
+            <span onClick={() => setCurrSate("Sign Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <span onClick={() => setCurrSate("Login")}>Login here</span>
+          </p>
+        )}
       </from>
     </div>
   );
