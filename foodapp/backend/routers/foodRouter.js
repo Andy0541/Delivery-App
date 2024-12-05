70a2c70311d909f3d5e0ff9f66054caadfd8e006
Andy Ríos <anrioss05@gmail.com>
2024-12-05 01:25:02 -0600

    API listo para un post en base

--- foodapp/backend/routers/foodRouter.js
+++ foodapp/backend/routers/foodRouter.js
@@ -4,4 +4,16 @@ import multer from "multer";
 
 const router = express.Router();
 
+//Image Storage Engine
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+foodRouter.post("/add", upload.single("image"), addFood);
+
 export default router;
