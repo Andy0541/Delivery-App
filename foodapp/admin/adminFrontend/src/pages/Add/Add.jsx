244616c24f15974c0a157bd10572c1124747d7e7
Andy Ríos <anrioss05@gmail.com>
2025-01-04 20:31:44 -0600

    Modificaciones en Add items

--- foodapp/admin/adminFrontend/src/pages/Add/Add.jsx
+++ foodapp/admin/adminFrontend/src/pages/Add/Add.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import "./Add.css";
 import { assets } from "../../assets/admin_assets/assets";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Add = () => {
+  const url = "http://localhost:4000";
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: "",
@@ -16,15 +19,47 @@ const Add = () => {
     setData((data) => ({ ...data, [name]: value }));
   };
 
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append("price", data.price);
+    formData.append("category", data.category);
+    formData.append("image", image);
+    const response = await axios.post(`${url}/api/food/add`, formData);
+    if (response.data.success) {
+      setData({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad",
+      });
+      setImage(false);
+      toast.success(response.data.message);
+    } else {
+      toast.error(response.data.message);
+    }
+  };
+
   return (
     <div className="add">
-      <from className="flex-col">
+      <form className="flex-col" onSubmit={onSubmitHandler}>
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
-            <img src={assets.upload_area} alt="" />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
+              alt=""
+            />
           </label>
-          <input type="file" id="image" hidden required />
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            id="image"
+            hidden
+            required
+          />
         </div>
         <div className="add-product-name flex-col">
           <p>Product Name</p>
@@ -77,7 +112,7 @@ const Add = () => {
         <button type="submit" className="add-btn">
           ADD
         </button>
-      </from>
+      </form>
     </div>
   );
 };
