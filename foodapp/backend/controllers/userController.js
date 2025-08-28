3c1c2d1b8cfe9b9f8362ef50693b0a2553a3d28a
Andy Ríos <anrioss05@gmail.com>
2025-08-28 01:37:26 -0600

    Verificar usuario

--- foodapp/backend/controllers/userController.js
+++ foodapp/backend/controllers/userController.js
@@ -4,8 +4,18 @@ import bcrypt from "bcrypt";
 import validator from "validator";
 
 // Login user
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  const { name, password, email } = req.body;
+  try {
+    // checking is user already exists
+    const exist = await userModel.findOne({ email });
+    if (exist) {
+      return res.json({ success: false, message: "User alredy exists" });
+    }
+  } catch (error) {}
+};
 
+// Register user
 const registerUser = async (req, res) => {};
 
 export { loginUser, registerUser };
